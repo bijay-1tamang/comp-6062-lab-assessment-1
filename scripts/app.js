b2ac87f22bb7f42aac8c3735a9ca7bda929218d5
bijay-1tamang <79555398+bijay-1tamang@users.noreply.github.com>
2024-06-11 19:25:54 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,7 @@ const studentNumber = "1154366";
 const result = `${studentName} - ${studentNumber}`;
 console.log(result);
 
+
 const headerContent = document.querySelector('h1');
 
 headerContent.innerHTML = `${studentName} - ${studentNumber}`;

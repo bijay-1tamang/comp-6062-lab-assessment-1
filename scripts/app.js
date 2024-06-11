19da358907fdcf03695da7ba4f0af95626898154
bijay-1tamang <79555398+bijay-1tamang@users.noreply.github.com>
2024-06-11 18:58:50 -0400

    Create app.js

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+console.log('scripts loaded!');
+
+const studentName = BijayTamang;
+const studentNumber = 1154366;
+
+const result = 'studentName-${studentNumber}';
+console.log(result);
+
+let headerContent = document.querySelector("#header");
